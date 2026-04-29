0bcab96a70656ae64541cc7578622bdf1cc06c44
quangtm <tranminhquang311097@gmail.com>
2026-04-29 09:51:12 +0700

    validate PUT /users

--- src/main/java/com/example/interviewtest/UserController.java
+++ src/main/java/com/example/interviewtest/UserController.java
@@ -1,15 +1,25 @@
 package com.example.interviewtest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<String> createUser() {
         return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<String> modifyUser() {
+        log.info("validation");
+
+        return ResponseEntity.status(HttpStatus.OK).body("User modified successfully");
     }
 }
